8e6d5f6d765df027a5b60e5bbbf25ecc8ff2f867
coed95 <codecoed95@gmail.com>
2024-06-06 17:20:35 +0200

    template
    Add script: "jest"

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html',
-            title: 'Restaurant Page',
+            title: 'template',
         }),
     ],
 
